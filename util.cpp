4f5eb5f495ae50767ecc48e4670bfb5eb65e73f5
Kythyria Tieran <kythyria@berigora.net>
2017-02-26 12:58:34 +0000

    Get a null-terminated wchar_t* in the fail dialog rather than an unterminated one.

--- util.cpp
+++ util.cpp
@@ -4,10 +4,8 @@
 bool AlertHresult(long hr, QString failmessage) {
     if(FAILED(hr)) {
         QString failmsg = failmessage.arg(hr);
-        wchar_t *msgdata = new wchar_t[failmsg.length()];
-        failmsg.toWCharArray(msgdata);
+        auto msgdata = (wchar_t*)failmsg.utf16();
         MessageBox(NULL, msgdata, L"Volume Control", MB_OK);
-        delete[] msgdata;
         return false;
     }
     
