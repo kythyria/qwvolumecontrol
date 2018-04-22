08a91a482cdbbe10f7b2f13da0f31a6066b09212
Kythyria Tieran <kythyria@berigora.net>
2018-04-22 04:29:08 +0100

    That constant was in the wrong place.

--- devicevolumemodel.cpp
+++ devicevolumemodel.cpp
@@ -9,6 +9,8 @@
 COM_SMARTPTR(IAudioEndpointVolume);
 COM_SMARTPTR(IPropertyStore);
 
+const PROPERTYKEY PKEY_AudioEndpoint_PhysicalSpeakers = { { 0x1da5d803, 0xd492, 0x4edd, { 0x8c, 0x23, 0xe0, 0xc0, 0xff, 0xee, 0x7f, 0x0e } }, 3 };
+
 class DeviceVolumeModel::Internal : public IAudioEndpointVolumeCallback {
     DeviceVolumeModel *owner;
     ULONG refcount;
--- volumesliderwidgetinternal.cpp
+++ volumesliderwidgetinternal.cpp
@@ -16,8 +16,6 @@
 #define SCALE 500
 #define SCALEF SCALE##.0f
 
-const PROPERTYKEY PKEY_AudioEndpoint_PhysicalSpeakers = { { 0x1da5d803, 0xd492, 0x4edd, { 0x8c, 0x23, 0xe0, 0xc0, 0xff, 0xee, 0x7f, 0x0e } }, 3 };
-
 VolumeSliderWidget::Internal::Internal(AbstractVolumeModel *model, VolumeSliderWidget *parent)
     : QObject(parent), model(0), linked(false)
 {
